2d5241717eb44370a3e3c20b5cb216654d74c909
nivedita-a <149430846+nivedita-a@users.noreply.github.com>
2025-01-19 19:15:46 +0530

    First Commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import JobListing from '../components/JobListing'
+import AppDowload from '../components/AppDowload'
+import Footer from '../components/Footer'
+
+const Home = () => {
+
+  
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <JobListing/>
+      <AppDowload/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
